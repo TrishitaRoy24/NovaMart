93d3c9e1916e8aa615c0b2e91b20b9287cfad9aa
TrishitaRoy24 <roytrishita24@gmail.com>
2026-06-23 23:31:06 +0530

    something went wrong page

--- novamart-frontend/src/app/features/products/pages/product-list/product-list.component.ts
+++ novamart-frontend/src/app/features/products/pages/product-list/product-list.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { ProductService } from '../../../../core/services/product.service';
 import { ProductCardComponent } from '../../../../shared/components/product-card/product-card.component';
+import { SomethingWentWrongComponent } from '../../../../shared/components/something-went-wrong/something-went-wrong.component';
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, SomethingWentWrongComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
---
+++ novamart-frontend/src/app/shared/components/something-went-wrong/something-went-wrong.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'something-went-wrong',
+  imports: [RouterLink],
+  templateUrl: './something-went-wrong.component.html',
+  styleUrl: './something-went-wrong.component.scss',
+})
+export class SomethingWentWrongComponent {}
