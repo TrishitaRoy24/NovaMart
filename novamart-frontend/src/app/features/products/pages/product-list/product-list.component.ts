a67f2af857e4955655394cf0380a1d68de475292
TrishitaRoy24 <roytrishita24@gmail.com>
2026-06-23 23:15:53 +0530

    product loader

--- novamart-frontend/src/app/features/products/pages/product-list/product-list.component.ts
+++ novamart-frontend/src/app/features/products/pages/product-list/product-list.component.ts
@@ -1,32 +1,44 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { ProductService } from '../../../../core/services/product.service';
-import { JsonPipe } from '@angular/common';
 import { ProductCardComponent } from '../../../../shared/components/product-card/product-card.component';
 
 @Component({
   selector: 'app-product-list',
-  imports: [JsonPipe, ProductCardComponent],
+  imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   productService = inject(ProductService);
 
+  $destroy = new Subject<void>();
+
   load = false;
+  isError = false;
   productList: any;
 
   ngOnInit(): void {
-    this.productService.getProductList().subscribe({
-      next: (result: any) => {
-        console.log('Products:', result.data);
-        this.productList = result.data;
-      },
-      error: (err: any) => {
-        this.load = true;
-      },
-      complete: () => {
-        this.load = true;
-      },
-    });
+    this.productService
+      .getProductList()
+      .pipe(takeUntil(this.$destroy))
+      .subscribe({
+        next: (result: any) => {
+          console.log('Products:', result.data);
+          this.productList = result.data;
+        },
+        error: (err: any) => {
+          this.load = true;
+          this.isError = true;
+        },
+        complete: () => {
+          this.load = true;
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.$destroy.next();
+    this.$destroy.complete();
   }
 }
